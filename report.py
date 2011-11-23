5ee17feb5a2238a1f3449972b622692cf7c0375b
Valentin Lab <valentin.lab@kalysto.org>
2011-11-23 19:14:17 +0100

    chg: use the new argument of the ``additional_data``

--- report.py
+++ report.py
@@ -19,8 +19,8 @@ from osv.osv import except_osv
 
 from report_xml import xml_report
 
-
 from .xmlrpc import GraphaneXMLRPC
+
 
 class GraphaneParser(xml_report.XmlParser):
     """Custom class that dump data to XML to send it to Graphane PDF generator
@@ -36,11 +36,6 @@ class GraphaneParser(xml_report.XmlParser):
         ## should return the raw data of a pdf
         return None
 
-    ## Override the default additional header from the ReportXml
-    ## report_xml.xml_full_dump_additional_headers
-    def _get_additional_data(self, cr, uid, ids, data, report_graphane, context=None):
-        return report_graphane.graphane_header
-
     # override needed to keep the attachments' storing procedure
     def create_single_pdf(self, cr, uid, ids, data, report_xml, context=None):
         """Override of inherited function to divert it and generate the XML
@@ -53,8 +48,9 @@ class GraphaneParser(xml_report.XmlParser):
             return super(GraphaneParser,self).create_single_pdf(cr, uid, ids, data, report_xml, context=context)
 
         ## XXXvlab: who calls this function
-        xml_content, _ =  self._create_full_dump_xml(cr, uid, ids, data, report_xml, context)
-        server = GraphaneXMLRPC(report_xml.graphane_xmlrpc_url)
+        xml_content, _ =  self._create_full_dump_xml(cr, uid, ids, data, report_xml, context,
+                                                     additional_data=report_xml.graphane_print_header)
+        server = GraphaneXMLRPC(report_xml.graphane_print_xmlrpc_url)
         pdf_content = server.generate("input.xml", xml_content)
 
         return (pdf_content, 'pdf')
